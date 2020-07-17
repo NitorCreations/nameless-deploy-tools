08aed960ef0bd5ef89066fe0fb9d8eb3cbb5e74a
Pasi Niemi <pasi.niemi@nitorcreations.com>
2020-07-17 11:11:24 +0300

    Nasm repo no longer works on CentOS due to moving to a new package compression method. Use CentOS standard nasm instead

--- n_utils/includes/creatable-templates/jenkins/pre_install.sh
+++ n_utils/includes/creatable-templates/jenkins/pre_install.sh
@@ -3,5 +3,4 @@
 yum install -y https://rpm.nodesource.com/pub_8.x/el/7/x86_64/nodesource-release-el7-1.noarch.rpm
 curl http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo > /etc/yum.repos.d/jenkins.repo
 rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
-yum-config-manager --add-repo http://www.nasm.us/nasm.repo
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
