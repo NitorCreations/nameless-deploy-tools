3798545b6ed518c2757f3696a1fffd7dcfe74c4c
Pasi Niemi <pasi.niemi@nitor.com>
2020-08-20 08:45:33 +0300

    Nuitka can now follow all imports. Slower to compile, but ends up faster

--- bin/optimize-project-env.sh
+++ bin/optimize-project-env.sh
@@ -3,7 +3,7 @@
 sudo -H pip install -U nuitka
 ENV_SCRIPT="$(dirname $(dirname $(n-include hook.sh)))/nameless-dt-load-project-env.py"
 PROFILE_SCRIPT="$(dirname $(dirname $(n-include hook.sh)))/nameless-dt-enable-profile.py"
-python -m nuitka --recurse-to=n_utils.project_util --follow-import-to=n_utils.profile_util --follow-import-to=n_utils $ENV_SCRIPT
-python -m nuitka --follow-import-to=n_utils.profile_util --follow-import-to=n_utils $PROFILE_SCRIPT
+python -m nuitka --recurse-to=n_utils.project_util --follow-imports $ENV_SCRIPT
+python -m nuitka --follow-imports $PROFILE_SCRIPT
 sudo cp nameless-dt-load-project-env.bin $(which nameless-dt-load-project-env)
 sudo cp nameless-dt-enable-profile.bin $(which nameless-dt-enable-profile)
