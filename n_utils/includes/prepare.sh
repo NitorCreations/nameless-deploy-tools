ee9c807d5c59a9838669f70b5b85034a69b34855
Pasi Niemi <pasi.niemi@nitor.com>
2020-09-09 10:13:36 +0300

    Make sure /usr/local/bin is on PATH - used by python pip for hooks

--- n_utils/includes/prepare.sh
+++ n_utils/includes/prepare.sh
@@ -2,4 +2,5 @@
 if ! grep -e 'secure_path.*/usr/local/bin' /etc/sudoers > /dev/null; then
     sed -i 's/\(.*secure_path.*\)/\1:\/usr\/local\/bin/' /etc/sudoers
 fi
+echo "pathmunge /usr/local/bin after" >> /etc/profile.d/sh.local
 exit 0
