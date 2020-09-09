5bf5729dfe1fbce67d76ace4e5c752f987ebdbd7
Pasi Niemi <pasi.niemi@nitor.com>
2020-09-09 15:22:31 +0300

    This default may be surprising to some users. This will be left to individual bakes to set up

--- n_utils/includes/prepare.sh
+++ n_utils/includes/prepare.sh
@@ -2,5 +2,4 @@
 if ! grep -e 'secure_path.*/usr/local/bin' /etc/sudoers > /dev/null; then
     sed -i 's/\(.*secure_path.*\)/\1:\/usr\/local\/bin/' /etc/sudoers
 fi
-echo "ALWAYS_SET_PATH=yes" > /etc/default/runuser
 exit 0
