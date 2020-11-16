c8a823ea4cc954998067166de62ff7b00b50e913
Pasi Niemi <pasi.niemi@nitor.com>
2020-11-16 17:03:33 +0200

    Add test for mfa backups

--- n_utils/tests/mfa_utils_test.py
+++ n_utils/tests/mfa_utils_test.py
@@ -1,5 +1,9 @@
 import re
-from n_utils.mfa_utils import mfa_add_token, mfa_delete_token, mfa_generate_code
+import json
+from os import unlink
+from tempfile import NamedTemporaryFile
+from n_utils import _to_bytes
+from n_utils.mfa_utils import mfa_add_token, mfa_delete_token, mfa_generate_code, mfa_backup_tokens, mfa_decrypt_backup_tokens
 
 class MyArgs:
     def __init__(self):
@@ -7,12 +11,26 @@ class MyArgs:
         self.token_secret = "I65VU7K5ZQL7WB4E"
         self.token_arn = "arn:aws:token:adfd0987654321"
         self.force = True
+MY_ARGS = MyArgs()
 
 def test_add_token():
-    mfa_add_token(MyArgs())
+    mfa_add_token(MY_ARGS)
 
 def test_get_code():
-    assert re.match(r"[0-9]{6}", mfa_generate_code(MyArgs().token_name))
+    assert re.match(r"[0-9]{6}", mfa_generate_code(MY_ARGS.token_name))
+
+def test_backup():
+    with NamedTemporaryFile(delete=False) as out:
+        out.write(_to_bytes(mfa_backup_tokens("test")))
+    tokens = json.loads(mfa_decrypt_backup_tokens("test", out.name))
+    unlink(out.name)
+    found = False
+    for token in tokens:
+        if token["token_name"] == MY_ARGS.token_name:
+            assert token["token_arn"] == MY_ARGS.token_arn
+            assert token["token_secret"] == MY_ARGS.token_secret
+            found = True
+    assert found
 
 def test_delete_token():
     mfa_delete_token(MyArgs().token_name)
