2d8af8ca00b7112d53d18270c3369f824a4431cd
Pasi Niemi <pasi.niemi@nitor.com>
2021-05-08 21:09:01 +0300

    Upgrade ec2-utils

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(name='nameless-deploy-tools',
           'configparser',
           'scandir',
           'jmespath',
-          'ec2-utils==0.26',
+          'ec2-utils==0.27',
           'cloudformation-utils==0.0.2',
           'PyYAML==5.2',
           'pyOpenSSL>=19.1.0'
