f987dd824a04224df95e20e647cbe07efd37a706
Pasi Niemi <pasi.niemi@nitorcreations.com>
2021-05-11 11:20:07 +0300

    Drop tests for python 3.5 as it is deprecated, it's installed base is limited at this point and keeping up with breaking libraries is a unnecessary burden

--- setup.py
+++ setup.py
@@ -22,11 +22,7 @@ if sys.version_info[0] == 2:
 elif sys.version_info[0] == 3:
     python2_or_3_deps = ['pyotp', 'Pygments', 'PyYAML>=5.2']
     python2_or_3_test_deps = ['pytest-mock', 'mock']
-    if sys.version_info[1] == 5:
-        python2_or_3_test_deps.insert(0, "pytest==6.1.2")
-        python2_or_3_test_deps.append('importlib-metadata==2.1.1')
-    else:
-        python2_or_3_test_deps.insert(0, "pytest")
+    python2_or_3_test_deps.insert(0, "pytest")
 if sys.platform.startswith('win'):
     win_deps = ['win-unicode-console', 'wmi', 'pypiwin32' ]
 
