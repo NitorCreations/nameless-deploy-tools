cfdd1eaf7a652d91dc98a6306f4342b2e6fe933f
Pasi Niemi <pasi.niemi@nitor.com>
2021-05-17 20:29:56 +0300

    Do not install as root and require nuitka and tools to be pre-installed (ccache and chrpath)

--- bin/optimize-project-env.sh
+++ bin/optimize-project-env.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo -H pip install -U nuitka
 ENV_SCRIPT="$(dirname $(dirname $(n-include hook.sh)))/nameless-dt-load-project-env.py"
 PROFILE_SCRIPT="$(dirname $(dirname $(n-include hook.sh)))/nameless-dt-enable-profile.py"
 python -m nuitka --follow-imports $ENV_SCRIPT
 python -m nuitka --follow-imports $PROFILE_SCRIPT
-sudo cp nameless-dt-load-project-env.bin $(which nameless-dt-load-project-env)
-sudo cp nameless-dt-enable-profile.bin $(which nameless-dt-enable-profile)
+cp nameless-dt-load-project-env.bin $(which nameless-dt-load-project-env)
+cp nameless-dt-enable-profile.bin $(which nameless-dt-enable-profile)
