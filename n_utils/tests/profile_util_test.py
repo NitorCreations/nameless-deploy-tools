c731f4f4e4d20e4c5dca8186ae40acd3cc92f77c
Pasi Niemi <pasi.niemi@nitor.com>
2022-01-06 15:07:49 +0200

    Add a few profile util tests

--- n_utils/tests/profile_util_test.py
+++ n_utils/tests/profile_util_test.py
@@ -1,4 +1,4 @@
-from n_utils.profile_util import get_profile, read_profiles
+from n_utils.profile_util import get_profile, read_profiles, enable_profile
 import json
 
 
@@ -10,3 +10,27 @@ def test_get_profile():
 def test_read_profiles():
     profiles = read_profiles()
     print(json.dumps(profiles))
+
+
+def test_iam_profile_switch(capsys):
+    enable_profile("iam", "testprofile")
+    captured = capsys.readouterr()
+    assert 'AWS_PROFILE="testprofile"' in captured.out
+
+
+def test_azure_profile_switch(capsys):
+    enable_profile("azure", "testprofile")
+    captured = capsys.readouterr()
+    assert "aws-azure-login --profile testprofile" in captured.out
+
+
+def test_adfs_profile_switch(capsys):
+    enable_profile("adfs", "testprofile")
+    captured = capsys.readouterr()
+    assert "adfs-aws-login --profile testprofile" in captured.out
+
+
+def test_adfs_profile_switch(capsys):
+    enable_profile("lastpass", "testprofile")
+    captured = capsys.readouterr()
+    assert "lastpass-aws-login --profile testprofile"
