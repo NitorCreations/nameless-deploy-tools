d6ad2a1c92e783230cf4a7fa3e83b400cf9bc1c0
Pasi Niemi <pasi.niemi@nitor.com>
2022-03-27 20:25:19 +0300

    Fix multiarch docker builds

---
+++ build-docker-base.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set +x
+bw get item hub.docker.com | jq -e -r '.fields[]|select(.name == "Access Token").value' | docker login -u "$(bw get username hub.docker.com)" --password-stdin
+set -x
+docker buildx build --platform linux/arm64 -t docker-base-arm64 docker-base
+docker buildx build --platform linux/amd64 -t docker-base-amd64 docker-base
+docker tag docker-base-arm64:latest nitor/docker-base:latest-arm64
+docker tag docker-base-amd64:latest nitor/docker-base:latest-amd64
+docker push nitor/docker-base:latest-arm64
+docker push nitor/docker-base:latest-amd64
+docker manifest create nitor/docker-base:latest nitor/docker-base:latest-arm64 nitor/docker-base:latest-amd64
+docker manifest annotate --arch amd64 nitor/docker-base:latest nitor/docker-base:latest-arm64
+docker manifest annotate --arch arm64 nitor/docker-base:latest nitor/docker-base:latest-amd64
+docker manifest push nitor/docker-base:latest
--- build-docker.sh
+++ build-docker.sh
@@ -2,24 +2,24 @@
 
 NEW_VERSION=$(grep nameless-deploy-tools docker/Dockerfile | cut -d "=" -f 3)
 
-docker buildx build --platform linux/amd64 --tag ndt docker
+docker buildx build --platform linux/amd64 --tag ndt-amd64 docker
+docker buildx build --platform linux/arm64 --tag ndt-arm64 docker
 set +x
 bw get item hub.docker.com | jq -e -r '.fields[]|select(.name == "Access Token").value' | docker login -u "$(bw get username hub.docker.com)" --password-stdin
 set -x
-docker tag ndt:latest nitor/ndt:$NEW_VERSION
-docker push nitor/ndt:$NEW_VERSION
-if ! echo "$NEW_VERSION" | grep "a" > /dev/null; then
-  docker tag ndt:latest nitor/ndt:latest
-  docker push nitor/ndt:latest
-fi
-docker buildx build --platform linux/arm64 --tag ndt docker
-set +x
-bw get item hub.docker.com | jq -e -r '.fields[]|select(.name == "Access Token").value' | docker login -u "$(bw get username hub.docker.com)" --password-stdin
-set -x
-docker tag ndt:latest nitor/ndt:$NEW_VERSION
-docker push nitor/ndt:$NEW_VERSION
-if ! echo "$NEW_VERSION" | grep "a" > /dev/null; then
-  docker tag ndt:latest nitor/ndt:latest
-  docker push nitor/ndt:latest
-fi
+docker tag ndt-amd64:latest nitor/ndt:amd64-$NEW_VERSION
+docker tag ndt-arm64:latest nitor/ndt:arm64-$NEW_VERSION
+docker push nitor/ndt:amd64-$NEW_VERSION
+docker push nitor/ndt:arm64-$NEW_VERSION
 
+docker manifest create nitor/ndt:$NEW_VERSION nitor/ndt:amd64-$NEW_VERSION nitor/ndt:arm64-$NEW_VERSION
+docker manifest annotate --arch amd64 nitor/ndt:$NEW_VERSION nitor/ndt:amd64-$NEW_VERSION
+docker manifest annotate --arch arm64 nitor/ndt:$NEW_VERSION nitor/ndt:arm64-$NEW_VERSION
+docker manifest push nitor/ndt:$NEW_VERSION
+
+if ! echo "$NEW_VERSION" | grep "a" > /dev/null; then
+  docker manifest create nitor/ndt:latest nitor/ndt:amd64-$NEW_VERSION nitor/ndt:arm64-$NEW_VERSION
+  docker manifest annotate --arch amd64 nitor/ndt:latest nitor/ndt:amd64-$NEW_VERSION
+  docker manifest annotate --arch arm64 nitor/ndt:latest nitor/ndt:arm64-$NEW_VERSION
+  docker manifest push nitor/ndt:latest
+fi
