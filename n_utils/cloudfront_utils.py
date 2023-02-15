0f15400797be294ef6b086f99c3cf11a67fba11d
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-02-15 18:30:54 +0200

    move license before imports

--- n_utils/cloudfront_utils.py
+++ n_utils/cloudfront_utils.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import time
-
 # Copyright 2017 Nitor Creations Oy
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +13,8 @@ import time
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License
+
+import time
 
 from threadlocal_aws.clients import cloudfront, route53
 
