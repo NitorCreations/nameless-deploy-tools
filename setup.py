d5f8af0289d48e04763180ea67be52cfc16dd0f3
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-02-27 15:50:27 +0200

    move to setup.cfg

--- setup.py
+++ setup.py
@@ -14,65 +14,6 @@
 
 # https://setuptools.pypa.io/en/latest/userguide/dependency_management.html
 
-import sys
-
 from setuptools import setup
-
-from n_utils import CONSOLESCRIPTS, PATH_COMMANDS
-
-win_deps = ["win-unicode-console", "wmi", "pypiwin32"]
-
-with open("README.md") as f:
-    long_description = f.read()
-
-setup(
-    name="nameless-deploy-tools",
-    python_requires=">=3.6",
-    version="1.283",
-    description="Tools for deploying to AWS via CloudFormation and Serverless framework that support a pull request based workflow",  # noqa
-    url="http://github.com/NitorCreations/nameless-deploy-tools",
-    download_url="https://github.com/NitorCreations/nameless-deploy-tools/tarball/1.283",
-    author="Pasi Niemi",
-    author_email="pasi@nitor.com",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="Apache 2.0",
-    packages=["n_utils"],
-    include_package_data=True,
-    scripts=PATH_COMMANDS,
-    entry_points={
-        "console_scripts": CONSOLESCRIPTS,
-    },
-    install_requires=[
-        "argcomplete",
-        "boto3",
-        "cloudformation-utils==0.0.2",
-        "configparser",
-        "ec2-utils>=0.38",
-        "ipaddr",
-        "jmespath",
-        "nitor-vault>=0.41",
-        "pyaml",
-        "pycryptodomex",
-        "Pygments",
-        "pyOpenSSL>=19.1.0",
-        "pyotp",
-        "pyqrcode",
-        "python-dateutil",
-        "PyYAML>=5.2",
-        "requests",
-        "scandir",
-        "six",
-        "termcolor",
-    ]
-    + (win_deps if sys.platform.startswith("win") else []),
-    tests_require=[
-        "coverage",
-        "mock",
-        "pytest-cov",
-        "pytest-mock",
-        "pytest",
-        "tomli<2.0.0",
-    ],
-    zip_safe=False,
-)
+if __name__ == '__main__':
+    setup()
