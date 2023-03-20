9a5d744987795b04bec53edc379bf57a4049805e
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-03-20 13:08:38 +0200

    c++ improvements

--- n_utils/project_util.py
+++ n_utils/project_util.py
@@ -138,23 +138,23 @@ def ndt_register_complete():
     local COMP_PREV="${COMP_WORDS[COMP_CWORD-1]}"
     local SUPPRESS_SPACE=0
     if compopt +o nospace 2> /dev/null; then
-        SUPPRESS_SPACE=1
+      SUPPRESS_SPACE=1
     fi
     COMPREPLY=( $(IFS="$IFS" \\
-                  COMP_LINE="$COMP_LINE" \\
-                  COMP_POINT="$COMP_POINT" \\
-                  COMP_TYPE="$COMP_TYPE" \\
-                  COMP_CUR="$COMP_CUR" \\
-                  COMP_PREV="$COMP_PREV" \\
-                  COMP_CWORD=$COMP_CWORD \\
-                  _ARGCOMPLETE_COMP_WORDBREAKS="$COMP_WORDBREAKS" \\
-                  _ARGCOMPLETE=1 \\
-                  _ARGCOMPLETE_SUPPRESS_SPACE=$SUPPRESS_SPACE \\
-                  "$1" 8>&1 9>&2 1>/dev/null 2>/dev/null) )
+      COMP_LINE="$COMP_LINE" \\
+      COMP_POINT="$COMP_POINT" \\
+      COMP_TYPE="$COMP_TYPE" \\
+      COMP_CUR="$COMP_CUR" \\
+      COMP_PREV="$COMP_PREV" \\
+      COMP_CWORD=$COMP_CWORD \\
+      _ARGCOMPLETE_COMP_WORDBREAKS="$COMP_WORDBREAKS" \\
+      _ARGCOMPLETE=1 \\
+      _ARGCOMPLETE_SUPPRESS_SPACE=$SUPPRESS_SPACE \\
+      "$1" 8>&1 9>&2 1>/dev/null 2>/dev/null) )
     if [[ $? != 0 ]]; then
-        unset COMPREPLY
+      unset COMPREPLY
     elif [[ $SUPPRESS_SPACE == 1 ]] && [[ "$COMPREPLY" =~ [=/:]$ ]]; then
-        compopt -o nospace
+      compopt -o nospace
     fi
 }
 complete -o nospace -F _ndt_complete "ndt"
