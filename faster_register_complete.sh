a94072aef445c0ee8c9aeaffc31480036b6bfe1d
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-03-20 13:08:38 +0200

    c++ improvements

--- faster_register_complete.sh
+++ faster_register_complete.sh
@@ -1,1 +1,59 @@
-g++ -std=c++11 -O2 n_utils/nameless-dt-register-complete.cpp -o $(dirname $(which python))/nameless-dt-register-complete
+#!/bin/bash
+set -eo pipefail
+
+REPO_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Check platform
+case "$(uname -s)" in
+"Darwin")
+  PLATFORM="mac"
+  ;;
+"MINGW"*)
+  PLATFORM="windows"
+  ;;
+*)
+  PLATFORM="linux"
+  ;;
+esac
+
+if [ -n "$(command -v python3)" ]; then
+  PYTHON=$(which python3)
+else
+  PYTHON=$(which python)
+fi
+
+if [ ! -e "$PYTHON" ]; then
+  echo "Python executable not found: $PYTHON"
+  exit 1
+else
+  echo "Using $PYTHON $($PYTHON --version)"
+fi
+
+ARGS=(-std=c++20 -O3 -Wall -Wextra)
+if [ "$PLATFORM" = mac ]; then
+  # 03/2023:
+  # try to use brew llvm / Clang since it is newer than what Apple includes.
+  # Critically, Clang 14 does not yet support `march` compiler option for Apple Silicon,
+  # but brew llvm comes with Clang 15 that does support it so we can get the full benefit from the C++ code.
+  # This can be removed once macOS comes with Clang 15 by default...
+  if [ -e "$(brew --prefix)/opt/llvm/bin/clang++" ]; then
+    COMPILER="$(brew --prefix)/opt/llvm/bin/clang++"
+    ARGS+=(-march=native -mtune=native)
+  else
+    COMPILER="g++"
+  fi
+else
+  COMPILER="g++"
+  ARGS+=(-march=native -mtune=native)
+fi
+
+ARGS+=("$REPO_ROOT/n_utils/nameless-dt-register-complete.cpp" -o "$(dirname "$PYTHON")/nameless-dt-register-complete")
+
+if [ -z "$(command -v "$COMPILER")" ]; then
+  echo "Compiler not found: $COMPILER"
+  exit 1
+fi
+
+echo "Running: $COMPILER ${ARGS[*]}"
+$COMPILER --version
+$COMPILER "${ARGS[@]}"
--- n_utils/nameless-dt-register-complete.cpp
+++ n_utils/nameless-dt-register-complete.cpp
@@ -1,6 +1,5 @@
-#include <string.h>
-
 #include <iostream>
+#include <string.h>
 
 const std::string base_str = R"delimiter(_ndt_complete() {
     local IFS=$'\013'
@@ -11,16 +10,16 @@ const std::string base_str = R"delimiter(_ndt_complete() {
         SUPPRESS_SPACE=1
     fi
     COMPREPLY=( $(IFS="$IFS" \
-                  COMP_LINE="$COMP_LINE" \
-                  COMP_POINT="$COMP_POINT" \
-                  COMP_TYPE="$COMP_TYPE" \
-                  COMP_CUR="$COMP_CUR" \
-                  COMP_PREV="$COMP_PREV" \
-                  COMP_CWORD=$COMP_CWORD \
-                  _ARGCOMPLETE_COMP_WORDBREAKS="$COMP_WORDBREAKS" \
-                  _ARGCOMPLETE=1 \
-                  _ARGCOMPLETE_SUPPRESS_SPACE=$SUPPRESS_SPACE \
-                  "$1" 8>&1 9>&2 1>/dev/null 2>/dev/null) )
+        COMP_LINE="$COMP_LINE" \
+        COMP_POINT="$COMP_POINT" \
+        COMP_TYPE="$COMP_TYPE" \
+        COMP_CUR="$COMP_CUR" \
+        COMP_PREV="$COMP_PREV" \
+        COMP_CWORD=$COMP_CWORD \
+        _ARGCOMPLETE_COMP_WORDBREAKS="$COMP_WORDBREAKS" \
+        _ARGCOMPLETE=1 \
+        _ARGCOMPLETE_SUPPRESS_SPACE=$SUPPRESS_SPACE \
+        "$1" 8>&1 9>&2 1>/dev/null 2>/dev/null) )
     if [[ $? != 0 ]]; then
         unset COMPREPLY
     elif [[ $SUPPRESS_SPACE == 1 ]] && [[ "$COMPREPLY" =~ [=/:]$ ]]; then
@@ -29,15 +28,17 @@ const std::string base_str = R"delimiter(_ndt_complete() {
 }
 complete -o nospace -F _ndt_complete "ndt"
 )delimiter";
+
 const std::string project_env_str = R"delimiter(_projectenv_hook() {
-  local previous_exit_status=$?;
-  eval "$(nameless-dt-load-project-env)";
-  return $previous_exit_status;
+    local previous_exit_status=$?;
+    eval "$(nameless-dt-load-project-env)";
+    return $previous_exit_status;
 };
 if ! [[ "$PROMPT_COMMAND" =~ _projectenv_hook ]]; then
-  PROMPT_COMMAND="_projectenv_hook;$PROMPT_COMMAND";
+    PROMPT_COMMAND="_projectenv_hook;$PROMPT_COMMAND";
 fi
 )delimiter";
+
 const std::string nep_complete_str = R"delimiter(_nep_complete() {
     COMPREPLY=( $(ndt print-aws-profiles "${COMP_WORDS[COMP_CWORD]}" ) )
 }
@@ -52,16 +53,18 @@ nep() {
 
 complete -F _nep_complete nep
 )delimiter";
-int main(int argc, char *argv[]) {
-  std::cout << base_str << std::endl;
-  if (argc > 1) {
-    for (uint32_t i = 1; i < argc; ++i) {
-      if (!strcmp(argv[i], "--project-env")) {
-        std::cout << project_env_str << std::endl;
-      }
-      if (!strcmp(argv[i], "--nep-function")) {
-        std::cout << nep_complete_str << std::endl;
-      }
+
+int main(int argc, char* argv[])
+{
+    std::cout << base_str << std::endl;
+    if (argc > 1) {
+        for (auto i = 1; i < argc; ++i) {
+            if (!strcmp(argv[i], "--project-env")) {
+                std::cout << project_env_str << std::endl;
+            }
+            if (!strcmp(argv[i], "--nep-function")) {
+                std::cout << nep_complete_str << std::endl;
+            }
+        }
     }
-  }
 }
