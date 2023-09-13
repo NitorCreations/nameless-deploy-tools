6ec11837bfa14a7d18221168bf083f25291c3066
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-09-13 13:55:34 +0300

    export variable to silence shellcheck unused warning

--- common.sh
+++ common.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 # Common shell functions and definitions
 
 REPO_ROOT=$(git rev-parse --show-toplevel || (cd "$(dirname "${BASH_SOURCE[0]}")" && pwd))
+export REPO_ROOT
 
 # Check platform
 case "$(uname -s)" in
