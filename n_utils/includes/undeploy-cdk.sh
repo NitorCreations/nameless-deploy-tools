f20a618a4ec3f0324df07ddfb7bb9ce9b9302723
Pasi Niemi <pasi.niemi@nitor.com>
2024-02-23 13:16:37 +0200

    Fix cdk undeploy

--- n_utils/includes/undeploy-cdk.sh
+++ n_utils/includes/undeploy-cdk.sh
@@ -18,17 +18,29 @@ if [ "$_ARGCOMPLETE" ]; then
   # Handle command completion executions
   unset _ARGCOMPLETE
   source $(n-include autocomplete-helpers.sh)
-  case $COMP_CWORD in
-    2)
-      compgen -W "-h $(get_stack_dirs)" -- $COMP_CUR
-      ;;
-    3)
-      compgen -W "$(get_cdk $COMP_PREV)" -- $COMP_CUR
-      ;;
-    *)
-      exit 1
-      ;;
-  esac
+  COMP_WORDS=( $COMP_LINE )
+
+  HELP="-h --help "
+  STACK_DIRS="$(get_stack_dirs) "
+  FOUND_STACK_DIR=""
+  for i in "${!COMP_WORDS[@]}"; do
+    if [ "$i" -lt 2 ]; then
+      continue
+    fi
+    word="${COMP_WORDS[$i]}"
+    if [ "$word" = "-h" -o "$word" = "--help" ]; then
+      HELP=""
+    elif [ -z "$FOUND_STACK_DIR" ]; then
+      if [ -d "$word" ]; then
+        FOUND_STACK_DIR="$word"
+        STACK_DIRS=""
+        SUBMODULES="$(get_cdk $FOUND_STACK_DIR)"
+      fi
+    else
+      SUBMODULES="$(get_cdk $FOUND_STACK_DIR)"
+    fi
+  done
+  compgen -W "${HELP}${STACK_DIRS}${SUBMODULES}" -- $COMP_CUR
   exit 0
 fi
 
@@ -52,21 +64,44 @@ usage() {
   fi
   exit 1
 }
-if [ "$1" = "--help" -o "$1" = "-h" ]; then
-  usage
-fi
+
 die() {
+  set +x
   echo "$1" >&2
   usage
+  exit 1
 }
-set -xe
 
-component="$1"
-shift
-[ "${component}" ] || die "You must give the component name as argument"
-cdk="$1"
-shift
-[ "${cdk}" ] || die "You must give the cdk name as argument"
+HELP=0
+ARGS=()
+
+# Iterate over all arguments
+while (( "$#" )); do
+  case "$1" in
+    -h|--help)
+      HELP=1
+      shift
+      ;;
+    *) # Preserve other arguments
+      ARGS+=("$1")
+      shift
+      ;;
+  esac
+done
+
+# If help is requested, show usage
+if [ "$HELP" -eq 1 ]; then
+  usage
+  exit 0
+fi
+
+# Process the remaining arguments
+if [ ${#ARGS[@]} -lt 2 ]; then
+  die "You must specify both the component and the cdk name."
+fi
+set -xe
+component="${ARGS[0]}"
+cdk="${ARGS[1]}"
 
 TSTAMP=$(date +%Y%m%d%H%M%S)
 if [ -z "$BUILD_NUMBER" ]; then
@@ -75,6 +110,8 @@ else
   BUILD_NUMBER=$(printf "%04d\n" $BUILD_NUMBER)
 fi
 
+eval "$(ndt load-parameters "$component" -c "$cdk" -e -r)"
+
 #If assume-deploy-role.sh is on the path, run it to assume the appropriate role for deployment
 if [ -n "$DEPLOY_ROLE_ARN" ] && [ -z "$AWS_SESSION_TOKEN" ]; then
   eval $(ndt assume-role $DEPLOY_ROLE_ARN)
@@ -82,10 +119,16 @@ elif which assume-deploy-role.sh &> /dev/null && [ -z "$AWS_SESSION_TOKEN" ]; th
   eval $(assume-deploy-role.sh)
 fi
 
-eval "$(ndt load-parameters "$component" -c "$cdk" -e)"
-
-ndt load-parameters "$component" -c "$cdk" -j > "$component/cdk-$ORIG_CDK_NAME/variables.json"
+ndt load-parameters "$component" -c "$cdk" -j -r > "$component/cdk-$ORIG_CDK_NAME/variables.json"
 
 cd "$component/cdk-$ORIG_CDK_NAME"
 
-cdk destroy
+if [ -x "./pre_deploy.sh" ]; then
+  "./pre_deploy.sh"
+fi
+
+if [ -r "./pre_deploy_source.sh" ]; then
+  source "./pre_deploy_source.sh"
+fi
+
+npx cdk destroy --ci --require-approval never
